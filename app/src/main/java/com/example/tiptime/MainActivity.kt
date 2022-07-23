f88bd48f384d6bedbe8ecd80206134df06883e00
shubham-sapkal <sapkal.shubham2002@gmail.com>
2022-07-23 11:49:33 +0530

    theming the app done

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -18,6 +22,9 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener{
             calculateTip()
         }
+
+        binding.costOfService.setOnKeyListener{ view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+
 
     }
 
@@ -52,6 +59,19 @@ class MainActivity : AppCompatActivity() {
 
         binding.tipTextView.text = getString(R.string.tip_amount, formattedTip)
 
+
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
 
     }
 
